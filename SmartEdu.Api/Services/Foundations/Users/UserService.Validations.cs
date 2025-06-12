f0d3ed1ea9585c1dd3bfcab7264a550d83b93f47
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-12 10:29:59 +0500

    CODE RUB: Implement TryCatch & Proper Validation

---
+++ SmartEdu.Api/Services/Foundations/Users/UserService.Validations.cs
@@ -0,0 +1,16 @@
+﻿using SmartEdu.Api.Models.Foundations.Users;
+using SmartEdu.Api.Models.Foundations.Users.Exceptions;
+
+namespace SmartEdu.Api.Services.Foundations.Users
+{
+    public partial class UserService
+    {
+        private void ValidateUserNotNull(User user)
+        {
+            if (user is null)
+            {
+                throw new NullUserException();
+            }
+        }
+    }
+}
--- SmartEdu.Api/Services/Foundations/Users/UserService.cs
+++ SmartEdu.Api/Services/Foundations/Users/UserService.cs
@@ -1,11 +1,10 @@
 ﻿using SmartEdu.Api.Brokers.Loggings;
 using SmartEdu.Api.Brokers.Storages;
 using SmartEdu.Api.Models.Foundations.Users;
-using SmartEdu.Api.Models.Foundations.Users.Exceptions;
 
 namespace SmartEdu.Api.Services.Foundations.Users
 {
-    public class UserService : IUserService
+    public partial class UserService : IUserService
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
@@ -18,26 +17,12 @@ namespace SmartEdu.Api.Services.Foundations.Users
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<User> AddUserAsync(User user)
+        public ValueTask<User> AddUserAsync(User user) =>
+        TryCatch(async () =>
         {
-            try
-            {
-                if (user is null)
-                {
-                    throw new NullUserException();
-                }
+            ValidateUserNotNull(user);
 
-                return await this.storageBroker.InsertUserAsync(user);
-            }
-            catch (NullUserException nullUserException)
-            {
-                var userValidationException =
-                    new UserValidationException(nullUserException);
-
-                this.loggingBroker.LogError(userValidationException);
-
-                throw userValidationException;
-            }
-        }
+            return await this.storageBroker.InsertUserAsync(user);
+        });
     }
 }
