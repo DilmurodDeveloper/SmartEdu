89b642fcfc045c7cea2361b2881bca5ffed8cb4f
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-13 00:12:10 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccureAndLogIt -> FAIL

---
+++ SmartEdu.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using SmartEdu.Api.Models.Foundations.Users.Exceptions;
+
+namespace SmartEdu.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccureAndLogIt()
+        {
+            //given
+            SqlException sqlException = GetSqlError();
+
+            var failedUserStorageException = new FailedUserStorageException(sqlException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(failedUserStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllUsers()).Throws(sqlException);
+
+            //when
+            Action retrieveAllUsersAction = () =>
+                this.userService.RetrieveAllUsers();
+
+            UserDependencyException userDependencyException =
+                Assert.Throws<UserDependencyException>(retrieveAllUsersAction);
+
+            //then
+            userDependencyException.Should().BeEquivalentTo(
+                expectedUserDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllUsers(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
