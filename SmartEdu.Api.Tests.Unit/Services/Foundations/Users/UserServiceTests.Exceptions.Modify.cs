bc7adbc1c7edbe418d44e8954bc4f1a3770956ae
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-14 00:47:25 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ SmartEdu.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using SmartEdu.Api.Models.Foundations.Users;
+using SmartEdu.Api.Models.Foundations.Users.Exceptions;
+
+namespace SmartEdu.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            User randomUser = CreateRandomUser();
+            User somUser = randomUser;
+            Guid userId = somUser.Id;
+            SqlException sqlException = GetSqlError();
+
+            var failedUserStorageException =
+                new FailedUserStorageException(sqlException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(failedUserStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(userId))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(somUser);
+
+            UserDependencyException actualUserDependencyException =
+                await Assert.ThrowsAsync<UserDependencyException>(modifyUserTask.AsTask);
+
+            //then
+            actualUserDependencyException.Should().BeEquivalentTo(
+                expectedUserDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(userId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
