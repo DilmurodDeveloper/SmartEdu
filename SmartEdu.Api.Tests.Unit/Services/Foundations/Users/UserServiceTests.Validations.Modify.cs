515c2d3e917cb9a5c8a10b98033f04b7548d07a2
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-13 23:19:01 +0500

    ShouldThrowValidationExceptionIfUserIsNullAndLogItAsync -> FAIL

---
+++ SmartEdu.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Modify.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using SmartEdu.Api.Models.Foundations.Users;
+using SmartEdu.Api.Models.Foundations.Users.Exceptions;
+
+namespace SmartEdu.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionIfUserIsNullAndLogItAsync()
+        {
+            //given
+            User nullUser = null;
+            var nullUserException = new NullUserException();
+
+            var expectedUserValidationException =
+                new UserValidationException(nullUserException);
+
+            //when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(nullUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                    modifyUserTask.AsTask);
+
+            //then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
